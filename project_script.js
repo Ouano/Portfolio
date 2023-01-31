c3bef18c29873c49f184406c077308c4c63b2497
17101233 <17101233@usc.edu.ph>
2023-01-31 22:28:31 +0800

    Img has been centered

--- project_script.js
+++ project_script.js
@@ -1,10 +1,16 @@
 const Java = document.getElementById("card6");
-const Kotlin = document.getElementById("card7")
+const Kotlin = document.getElementById("card7");
 
+
+var img = document.getElementById("showProject");
+var img2 = document.getElementById("showProject2");
+
+img.style.display = "block";
+img.style.margin = "auto";
+img2.style.display = "block";
+img2.style.margin = "auto";
 
 function JavaProject() {
-    var img = document.getElementById("showProject");
-    var img2 = document.getElementById("showProject2");
     img.src = "./img/profile.jpg";
     img2.src = "./img/aboutme.jpg";
     document.body.appendChild(img);
@@ -12,10 +18,9 @@ function JavaProject() {
 }
 
 function KotlinProject() {
-    var img = document.getElementById("showProject");
-    var img2 = document.getElementById("showProject2");
     img.src = "./img/aboutme.jpg";
     img2.src = "./img/profile.jpg";
+    
     document.body.appendChild(img);
     document.body.appendChild(img2);
 }
